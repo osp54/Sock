1eb4441a7abac2d24cbbc0febd3dfe31d651a055
nekonya <misakachan2545@gmail.com>
2023-05-12 18:47:43 +0300

    update

--- src/main/java/com/ospx/sock/PacketSerializer.java
+++ src/main/java/com/ospx/sock/PacketSerializer.java
@@ -6,11 +6,11 @@ import arc.util.Log;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.ByteBufferInput;
 import com.esotericsoftware.kryo.io.ByteBufferOutput;
-import com.esotericsoftware.kryo.io.KryoBufferUnderflowException;
 import org.objenesis.strategy.StdInstantiatorStrategy;
 
+import arc.net.FrameworkMessage.*;
+
 import java.nio.ByteBuffer;
-import java.util.Arrays;
 
 public class PacketSerializer implements NetSerializer {
     public final Kryo kryo = new Kryo();
@@ -23,54 +23,72 @@ public class PacketSerializer implements NetSerializer {
 
     @Override
     public void write(ByteBuffer buffer, Object object) {
-        try (ByteBufferOutput output = new ByteBufferOutput(buffer)) {
-            kryo.writeClass(output, object.getClass());
-            kryo.writeObject(output, object);
+        if(object instanceof FrameworkMessage m){
+            buffer.put((byte)-2);
+            writeFramework(buffer,m);
+        }else {
+            buffer.put((byte) -1);
+            try (ByteBufferOutput output = new ByteBufferOutput(buffer)) {
+                kryo.writeClass(output, object.getClass());
+                kryo.writeObject(output, object);
+            }
         }
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public Object read(ByteBuffer buffer) {
+        byte type = buffer.get();
 
-        var d = buffer.array();
-        if (d[0] == 0&&(d[1] >=0&&d[1] <=4)) {
+        if (type==-2){
             return readFramework(buffer);
+        }else{
+            try (ByteBufferInput input = new ByteBufferInput(buffer)) {
+                var registration = kryo.readClass(input);
+                if (registration == null) return null;
+                Log.info("<<< "+registration.getType().getName());
+                return kryo.readObject(input, registration.getType());
+            }
         }
+    }
 
-        try (ByteBufferInput input = new ByteBufferInput(buffer)) {
-            var registration = kryo.readClass(input);
-            if (registration == null) return null;
-
-            return kryo.readObject(input, registration.getType());
-        }catch (Exception e){
-            Log.err(e);
-            return null;
+    public void writeFramework(ByteBuffer buffer, FrameworkMessage message){
+        if(message instanceof Ping p){
+            buffer.put((byte)0);
+            buffer.putInt(p.id);
+            buffer.put(p.isReply ? 1 : (byte)0);
+        }else if(message instanceof DiscoverHost){
+            buffer.put((byte)1);
+        }else if(message instanceof KeepAlive){
+            buffer.put((byte)2);
+        }else if(message instanceof RegisterUDP p){
+            buffer.put((byte)3);
+            buffer.putInt(p.connectionID);
+        }else if(message instanceof RegisterTCP p){
+            buffer.put((byte)4);
+            buffer.putInt(p.connectionID);
         }
     }
 
     public FrameworkMessage readFramework(ByteBuffer buffer){
-        buffer.position(0);
-        buffer.get();
         byte id = buffer.get();
-        Log.info(id);
+
         if(id == 0){
-            FrameworkMessage.Ping p = new FrameworkMessage.Ping();
+            Ping p = new Ping();
             p.id = buffer.getInt();
             p.isReply = buffer.get() == 1;
             return p;
         }else if(id == 1){
             return FrameworkMessage.discoverHost;
         }else if(id == 2){
-            buffer.get();
-            buffer.get();
+            Log.info("KeepAlive");
             return FrameworkMessage.keepAlive;
         }else if(id == 3){
-            FrameworkMessage.RegisterUDP p = new FrameworkMessage.RegisterUDP();
+            RegisterUDP p = new RegisterUDP();
             p.connectionID = buffer.getInt();
             return p;
         }else if(id == 4){
-            FrameworkMessage.RegisterTCP p = new FrameworkMessage.RegisterTCP();
+            RegisterTCP p = new RegisterTCP();
             p.connectionID = buffer.getInt();
             return p;
         }else{
