962e44e1557e9a452954b0dabbef752bc1b5e437
OSP <76648940+osp54@users.noreply.github.com>
2023-07-14 16:53:08 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,23 +26,11 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.anuken.arc:arc-core:v145")
-    implementation("com.github.anuken.arc:arcnet:v145")
+    compileOnly("com.github.anuken.arc:arc-core:v145")
+    compileOnly("com.github.anuken.arc:arcnet:v145")
 
     implementation("com.alibaba:fastjson:2.0.32")
 
     compileOnly("org.projectlombok:lombok:1.18.26")
     annotationProcessor("org.projectlombok:lombok:1.18.26")
-}
-
-jar {
-    doFirst {
-        configurations.runtimeClasspath.each { file ->
-            def dependencyNotation = dependencies.create(file.absolutePath)
-            if (dependencyNotation.group == "com.github.anuken") {
-                dependencies.remove(dependencyNotation)
-                compileOnly dependencyNotation
-            }
-        }
-    }
 }
